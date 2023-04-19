bd7c891c9df216ebba574fdc67a85f92cc1bf045
jhosefin <90260920+jhosefin@users.noreply.github.com>
2023-04-19 18:10:13 -0400

    pruebas a mi rama

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 
 import { login } from './view/login.js';
+import { navbar } from './view/navbar.js';
 
 const main = document.getElementById('main');
+const header = document.getElementById('header')
 const btnLoginHome = document.getElementById('btnLoginHome');
 const txtEmail = document.getElementById("txtEmail");
 const txtPassword = document.getElementById('txtPassword');
@@ -10,5 +12,6 @@ const btnSignup = document.getElementById('btnSignup');
 const btnLogout = document.getElementById('btnLogout');
 
 btnLoginHome.addEventListener("click", () =>{
+    header.innerHTML= navbar();
     main.innerHTML= login();
 })
---
+++ src/view/navbar.js
@@ -0,0 +1,12 @@
+export const navbar = () => {
+    return `<header>
+    <img src="img/logo.png">
+    <nav>    
+      <ul>
+        <li id="btnLoginHome">
+          <a  href="#">Home</a>
+        </li>
+      </ul> 
+    </nav>
+   </header>`
+}
