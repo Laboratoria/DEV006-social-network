6eeb47c1f143406659a63f4579196f7102233d88
jhosefin <90260920+jhosefin@users.noreply.github.com>
2023-04-26 16:40:08 -0400

    Correcciones slint

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,9 +6,15 @@
 };*/
 
 import { firebaseConfig } from "../firebase.config.js";
-import { initializeApp } from 'firebase/app';
-import { getAuth,signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithCredential } from "firebase/auth";
-
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithCredential,
+} from "firebase/auth";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -17,61 +23,58 @@ const auth = getAuth(app);
 
 // Initialize Firebase Authentication and get a reference to the service
 
-
-export const signPop= async(event) => {
+export const signPop = async (event) => {
   event.preventDefault();
-const provider = new GoogleAuthProvider();
+  const provider = new GoogleAuthProvider();
   try {
-
     // Ejecutar la autenticación con Google
     const result = await signInWithPopup(auth, provider);
     const credential = GoogleAuthProvider.credentialFromResult(result);
-    console.log(credential)
+    console.log(credential);
 
     // Iniciar sesión en Firebase con las credenciales obtenidas
     const userCredential = await signInWithCredential(auth, credential);
     const user = userCredential.user;
     console.log(user);
 
-    await setDoc(doc(db, 'users', user.uid), {
+    await setDoc(doc(db, "users", user.uid), {
       email: user.email,
       displayName: user.displayName,
       photoURL: user.photoURL,
     });
-
-  } catch(error) {
-    console.log(error)
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
-
-
-export const createUser = async   ()  => {
-
-  const email = document.getElementById('txtEmail')
-  const password = document.getElementById('txtPassword')
-  const spanEmail = document.getElementById('spanErrorEmail')
-  const spanPassword = document.getElementById('spanErrorPassword')
+export const createUser = async () => {
+  const email = document.getElementById("txtEmail");
+  const password = document.getElementById("txtPassword");
+  const spanEmail = document.getElementById("spanErrorEmail");
+  const spanPassword = document.getElementById("spanErrorPassword");
   try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value)
-    
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email.value,
+      password.value
+    );
   } catch (error) {
     const errorCode = error.code;
     const errorMessage = error.message;
 
-    if(errorCode === 'auth/email-already-in-use'){
-      spanEmail.textContent = 'Email in use'
-    }else if (errorCode === 'auth/invalid-email'){
-      spanEmail.textContent = 'Invalid Email'
-    }else if(errorCode === 'auth/weak-password'){
-      spanPassword.textContent = 'Password is too weak'
+    if (errorCode === "auth/email-already-in-use") {
+      spanEmail.textContent = "Email in use";
+    } else if (errorCode === "auth/invalid-email") {
+      spanEmail.textContent = "Invalid Email";
+    } else if (errorCode === "auth/weak-password") {
+      spanPassword.textContent = "Password is too weak";
     }
   }
 };
 
 export const LoginUser = () => {
-  const emailInput = document.getElementById('txtEmail');
-  const passwordInput = document.getElementById('txtPassword');
+  const emailInput = document.getElementById("txtEmail");
+  const passwordInput = document.getElementById("txtPassword");
 
   const email = emailInput.value;
   const password = passwordInput.value;
@@ -80,7 +83,7 @@ export const LoginUser = () => {
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
-      console.log(email)
+      console.log(email);
 
       // ...
     })
@@ -88,47 +91,48 @@ export const LoginUser = () => {
       const errorCode = error.code;
       const errorMessage = error.message;
 
-      if (errorCode === 'auth/user-not-found') {
-        emailInput.classList.add('invalid');
-        document.getElementById('spanErrorEmail').textContent = 'User not found';
-      } else if (errorCode === 'auth/wrong-password') {
-        passwordInput.classList.add('invalid');
-        document.getElementById('spanErrorPassword').textContent = 'Wrong password';
+      if (errorCode === "auth/user-not-found") {
+        emailInput.classList.add("invalid");
+        document.getElementById("spanErrorEmail").textContent =
+          "User not found";
+      } else if (errorCode === "auth/wrong-password") {
+        passwordInput.classList.add("invalid");
+        document.getElementById("spanErrorPassword").textContent =
+          "Wrong password";
       }
     });
 };
 
+export const securePassword = () => {
+  const password = document.getElementById("txtPassword");
 
-export const securePassword = () =>{
-  const password = document.getElementById('txtPassword')
-  
-  const paswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_][^\s]{6,15}$/;
+  const paswordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_][^\s]{6,15}$/;
 
   if (paswordRegex.test(password.value)) {
-      password.classList.remove('invalid');
-      password.classList.add('valid');
-      document.getElementById('spanErrorPassword').textContent = '';
+    password.classList.remove("invalid");
+    password.classList.add("valid");
+    document.getElementById("spanErrorPassword").textContent = "";
   } else {
-    password.classList.remove('valid');
-    password.classList.add('invalid');
-    document.getElementById('spanErrorPassword').textContent = 'Please enter a strong password that contains 6 to 15 characters, at least one uppercase letter, one lowercase letter, one digit, and one special character. Please make sure there are no spaces.';
+    password.classList.remove("valid");
+    password.classList.add("invalid");
+    document.getElementById("spanErrorPassword").textContent =
+      "Please enter a strong password that contains 6 to 15 characters, at least one uppercase letter, one lowercase letter, one digit, and one special character. Please make sure there are no spaces.";
   }
-
 };
 
-export const validateEmail = () =>{
-
-  const email = document.getElementById('txtEmail')
+export const validateEmail = () => {
+  const email = document.getElementById("txtEmail");
   const correoRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   if (correoRegex.test(email.value)) {
-      email.classList.remove('invalid');
-      email.classList.add('valid');
-      document.getElementById('spanErrorEmail').textContent = '';
+    email.classList.remove("invalid");
+    email.classList.add("valid");
+    document.getElementById("spanErrorEmail").textContent = "";
   } else {
-    email.classList.remove('valid');
-    email.classList.add('invalid');
-    document.getElementById('spanErrorEmail').textContent = 'Please enter a valid email.';
+    email.classList.remove("valid");
+    email.classList.add("invalid");
+    document.getElementById("spanErrorEmail").textContent =
+      "Please enter a valid email.";
   }
-
-}
+};
--- src/view/welcome.js
+++ src/view/welcome.js
@@ -1,162 +1,166 @@
 import { login } from "./login";
 
 export let welcome = () => {
-    const body= document.getElementById("root")
-    body.innerHTML= "";
-    
-    const bodyimg = document.createElement("div");
-    bodyimg.setAttribute("class", "bodyimg");
+  const body = document.getElementById("root");
+  body.innerHTML = "";
 
-    const header = document.createElement("header");
+  const bodyimg = document.createElement("div");
+  bodyimg.setAttribute("class", "bodyimg");
 
-    const logoImg = document.createElement("img");
-    logoImg.setAttribute("src", "img/logo.png");
+  const header = document.createElement("header");
 
-    const nav = document.createElement('nav');
+  const logoImg = document.createElement("img");
+  logoImg.setAttribute("src", "img/logo.png");
 
-    const ul = document.createElement('ul');
+  const nav = document.createElement("nav");
 
-    const btnLoginHome = document.createElement('li');
-    btnLoginHome.setAttribute('id', 'btnLoginHome');
+  const ul = document.createElement("ul");
 
-    const loginLink = document.createElement('a');
-    loginLink.setAttribute('href','/login');
-    loginLink.textContent = 'Log In';
+  const btnLoginHome = document.createElement("li");
+  btnLoginHome.setAttribute("id", "btnLoginHome");
 
-    const signUpLink = document.createElement('li');
+  const loginLink = document.createElement("a");
+  loginLink.setAttribute("href", "/login");
+  loginLink.textContent = "Log In";
 
-    const signUp = document.createElement('a');
-    signUp.setAttribute('href','/register');;
-    signUp.textContent = 'Sign Up';
+  const signUpLink = document.createElement("li");
 
-    const main = document.createElement('main');
-    main.setAttribute('id', 'main');
+  const signUp = document.createElement("a");
+  signUp.setAttribute("href", "/register");
+  signUp.textContent = "Sign Up";
 
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Be My Friend';
+  const main = document.createElement("main");
+  main.setAttribute("id", "main");
 
-    const welcomeMessage = document.createElement('p');
-    welcomeMessage.setAttribute('class','welcomemessage');
-    welcomeMessage.innerHTML = 'Welcome <br>Your next friend is waiting for you. Check animals available to adopt!';
+  const h1 = document.createElement("h1");
+  h1.textContent = "Be My Friend";
 
+  const welcomeMessage = document.createElement("p");
+  welcomeMessage.setAttribute("class", "welcomemessage");
+  welcomeMessage.innerHTML =
+    "Welcome <br>Your next friend is waiting for you. Check animals available to adopt!";
 
-    const sectionCards = document.createElement('section');
-    sectionCards.setAttribute('class','contenedorcards');
+  const sectionCards = document.createElement("section");
+  sectionCards.setAttribute("class", "contenedorcards");
 
-    const cardPet1 = document.createElement('div');
-    cardPet1.setAttribute('class','cardpet');
+  const cardPet1 = document.createElement("div");
+  cardPet1.setAttribute("class", "cardpet");
 
-    const photoPets1 = document.createElement('div');
-    photoPets1.setAttribute('class','photopets');
+  const photoPets1 = document.createElement("div");
+  photoPets1.setAttribute("class", "photopets");
 
-    const img1 = document.createElement('img');
-    img1.setAttribute('src','img/TOM.jpg');
+  const img1 = document.createElement("img");
+  img1.setAttribute("src", "img/TOM.jpg");
 
-    const interactionPets1 = document.createElement('div');
-    interactionPets1.setAttribute('class','interactionpets');
+  const interactionPets1 = document.createElement("div");
+  interactionPets1.setAttribute("class", "interactionpets");
 
-    const h3Pet1 = document.createElement('h3');
-    h3Pet1.textContent = 'TOM';
+  const h3Pet1 = document.createElement("h3");
+  h3Pet1.textContent = "TOM";
 
-    const inputLike1 = document.createElement('input');
-    inputLike1.setAttribute('type','image');
-    inputLike1.setAttribute('src','img/megusta.png');
-    inputLike1.setAttribute('alt','logomegusta');
-    inputLike1.addEventListener('click',login)
-    inputLike1.addEventListener('mouseover', function() {
-        this.src = "img/megustarojo.png";
-    });
-    inputLike1.addEventListener('mouseout', function() {
-        this.src = "img/megusta.png";
-    });
+  const inputLike1 = document.createElement("input");
+  inputLike1.setAttribute("type", "image");
+  inputLike1.setAttribute("src", "img/megusta.png");
+  inputLike1.setAttribute("alt", "logomegusta");
+  inputLike1.addEventListener("click", login);
+  inputLike1.addEventListener("mouseover", function () {
+    this.src = "img/megustarojo.png";
+  });
+  inputLike1.addEventListener("mouseout", function () {
+    this.src = "img/megusta.png";
+  });
 
-    const inputMatch1 = document.createElement('input');
-    inputMatch1.setAttribute('type','image');
-    inputMatch1.setAttribute('src','img/matchvacio.png');
-    inputMatch1.setAttribute('alt','logomatch');
-    inputMatch1.addEventListener('click',login)
-    inputMatch1.addEventListener('mouseover', function() {
-        this.src = "img/match.png";
-    });
-    inputMatch1.addEventListener('mouseout', function() {
-        this.src = "img/matchvacio.png";
-    });
+  const inputMatch1 = document.createElement("input");
+  inputMatch1.setAttribute("type", "image");
+  inputMatch1.setAttribute("src", "img/matchvacio.png");
+  inputMatch1.setAttribute("alt", "logomatch");
+  inputMatch1.addEventListener("click", login);
+  inputMatch1.addEventListener("mouseover", function () {
+    this.src = "img/match.png";
+  });
+  inputMatch1.addEventListener("mouseout", function () {
+    this.src = "img/matchvacio.png";
+  });
 
-    const cardPet2 = document.createElement('div');
-    cardPet2.setAttribute('class','cardpet');
+  const cardPet2 = document.createElement("div");
+  cardPet2.setAttribute("class", "cardpet");
 
-    const photoPets2 = document.createElement('div');
-    photoPets2.setAttribute('class','photopets');
+  const photoPets2 = document.createElement("div");
+  photoPets2.setAttribute("class", "photopets");
 
-    const img2 = document.createElement('img');
-    img2.setAttribute('src','img/Bob.png');
+  const img2 = document.createElement("img");
+  img2.setAttribute("src", "img/Bob.png");
 
-    const interactionPets2 = document.createElement('div');
-    interactionPets2.setAttribute('class','interactionpets');
+  const interactionPets2 = document.createElement("div");
+  interactionPets2.setAttribute("class", "interactionpets");
 
-    const h3Pet2 = document.createElement('h3');
-    h3Pet2.textContent = 'BOB';
+  const h3Pet2 = document.createElement("h3");
+  h3Pet2.textContent = "BOB";
 
-    const inputLike2 = document.createElement('input');
-    inputLike2.setAttribute('type','image');
-    inputLike2.setAttribute('src','img/megusta.png');
-    inputLike2.setAttribute('alt','logomegusta');
-    inputLike2.addEventListener('click',login)
-    inputLike2.addEventListener('mouseover', function() {
-        this.src = "img/megustarojo.png";
-    });
-    inputLike2.addEventListener('mouseout', function() {
-        this.src = "img/megusta.png";
-    });
+  const inputLike2 = document.createElement("input");
+  inputLike2.setAttribute("type", "image");
+  inputLike2.setAttribute("src", "img/megusta.png");
+  inputLike2.setAttribute("alt", "logomegusta");
+  inputLike2.addEventListener("click", login);
+  inputLike2.addEventListener("mouseover", function () {
+    this.src = "img/megustarojo.png";
+  });
+  inputLike2.addEventListener("mouseout", function () {
+    this.src = "img/megusta.png";
+  });
 
-    const inputMatch2 = document.createElement('input');
-    inputMatch2.setAttribute('type','image');
-    inputMatch2.setAttribute('src','img/matchvacio.png');
-    inputMatch2.setAttribute('alt','logomatch');
-    inputMatch2.addEventListener('click',login)
-    inputMatch2.addEventListener('mouseover', function() {
-        this.src = "img/match.png";
-    });
-    inputMatch2.addEventListener('mouseout', function() {
-        this.src = "img/matchvacio.png";
-    });
-    const sectionTestimonies = document.createElement('section');
-    sectionTestimonies.setAttribute('class','contenedortestimonios');
+  const inputMatch2 = document.createElement("input");
+  inputMatch2.setAttribute("type", "image");
+  inputMatch2.setAttribute("src", "img/matchvacio.png");
+  inputMatch2.setAttribute("alt", "logomatch");
+  inputMatch2.addEventListener("click", login);
+  inputMatch2.addEventListener("mouseover", function () {
+    this.src = "img/match.png";
+  });
+  inputMatch2.addEventListener("mouseout", function () {
+    this.src = "img/matchvacio.png";
+  });
+  const sectionTestimonies = document.createElement("section");
+  sectionTestimonies.setAttribute("class", "contenedortestimonios");
 
-    const h2Testimonies = document.createElement('h2');
-    h2Testimonies.setAttribute('class','testimonies');
-    h2Testimonies.textContent = 'Testimonies';
+  const h2Testimonies = document.createElement("h2");
+  h2Testimonies.setAttribute("class", "testimonies");
+  h2Testimonies.textContent = "Testimonies";
 
-    const messageTestimony1 = document.createElement('div');
-    messageTestimony1.setAttribute('class','menssagetesti');
+  const messageTestimony1 = document.createElement("div");
+  messageTestimony1.setAttribute("class", "menssagetesti");
 
-    const pTestimony1 = document.createElement('p');
-    pTestimony1.innerHTML = '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...”<br>-Name, City';
+  const pTestimony1 = document.createElement("p");
+  pTestimony1.innerHTML =
+    "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...”<br>-Name, City";
 
-    const messageTestimony2 = document.createElement('div');
-    messageTestimony2.setAttribute('class','menssagetesti');
+  const messageTestimony2 = document.createElement("div");
+  messageTestimony2.setAttribute("class", "menssagetesti");
 
-    const pTestimony2 = document.createElement('p');
-    pTestimony2.innerHTML = '“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...”<br>-Name, City';
+  const pTestimony2 = document.createElement("p");
+  pTestimony2.innerHTML =
+    "“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...”<br>-Name, City";
 
-    
-
-    body.append(bodyimg)
-    bodyimg.append(header,main);
-    header.append(logoImg,nav);
-    nav.append(ul);
-    ul.append(btnLoginHome, signUpLink);
-    btnLoginHome.append(loginLink);
-    signUpLink.append(signUp);
-    main.append(h1,welcomeMessage,sectionCards,sectionTestimonies);
-    sectionCards.append(cardPet1,cardPet2);
-    cardPet1.append(photoPets1,interactionPets1);
-    photoPets1.append(img1);
-    interactionPets1.append(h3Pet1,inputLike1,inputMatch1);
-    cardPet2.append(photoPets2,interactionPets2);
-    photoPets2.append(img2);
-    interactionPets2.append(h3Pet2,inputLike2,inputMatch2);
-    sectionTestimonies.append(h2Testimonies,messageTestimony1,messageTestimony2);
-    messageTestimony1.append(pTestimony1);
-    messageTestimony2.append(pTestimony2);
-}
+  body.append(bodyimg);
+  bodyimg.append(header, main);
+  header.append(logoImg, nav);
+  nav.append(ul);
+  ul.append(btnLoginHome, signUpLink);
+  btnLoginHome.append(loginLink);
+  signUpLink.append(signUp);
+  main.append(h1, welcomeMessage, sectionCards, sectionTestimonies);
+  sectionCards.append(cardPet1, cardPet2);
+  cardPet1.append(photoPets1, interactionPets1);
+  photoPets1.append(img1);
+  interactionPets1.append(h3Pet1, inputLike1, inputMatch1);
+  cardPet2.append(photoPets2, interactionPets2);
+  photoPets2.append(img2);
+  interactionPets2.append(h3Pet2, inputLike2, inputMatch2);
+  sectionTestimonies.append(
+    h2Testimonies,
+    messageTestimony1,
+    messageTestimony2
+  );
+  messageTestimony1.append(pTestimony1);
+  messageTestimony2.append(pTestimony2);
+};
