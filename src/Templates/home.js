ee06722f31038d23f4f97368aedf30c23e284458
Tamarindo <t.aviles.bosoni@gmail.com>
2023-05-03 09:17:44 -0400

    se a gregan templates

---
+++ src/Templates/home.js
@@ -0,0 +1,11 @@
+export const home= `
+<header>add logo </header>
+<p>Email</p>
+<textarea class="insertInfo" placeholder="example@gmail.com"></textarea>
+<p>Password</p>
+<textarea class="insertInfo" type="password" placeholder="Mypassword"></textarea> 
+<p>Forgot your password?</p>
+<button class="buttons">Log In</button>
+<button class="buttons">Sign Up</button>
+<button class="buttons">Continue with Google</button>
+`
