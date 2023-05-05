3937cf6fce9c7487b8d37311c2448f8eecad8931
lalailamas <ale.cordoba.p@gmail.com>
2023-05-05 13:12:46 -0400

    instalando firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import {login, logout} from "./auth.js";
+const buttonLogin = document.querySelector("#button-login");
+buttonLogin.addEventListener("click", async e => {
+  try {
+    await login ();
+  } catch (error){
 
-myFunction();
+  }
+});
