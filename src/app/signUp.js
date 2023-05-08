643ec68866dbf43b5ce0f101f14529e9914399f6
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-05-08 09:32:12 -0600

    firebase auth

---
+++ src/app/signUp.js
@@ -0,0 +1,20 @@
+import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
+import { auth } from './firebase.js';
+
+const signUpForm = document.querySelector('#signup-form');
+
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const email = signUpForm['email'].value;
+    const password = signUpForm['password'].value;
+
+    console.log(email, password)
+
+    try {
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(userCredentials);
+    }catch (error) {
+        console.log(error);
+      }
+    });
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // Este es el punto de entrada de tu aplicacion
+import './app/signUp.js';
 
-import {login, logout} from "./auth.js";
-const buttonLogin = document.querySelector("#button-login");
-buttonLogin.addEventListener("click", async e => {
-  try {
-    await login ();
-  } catch (error){
+// import {login, logout} from "./auth.js";
+// const buttonLogin = document.querySelector("#button-login");
+// buttonLogin.addEventListener("click", async e => {
+//   try {
+//     await login ();
+//   } catch (error){
 
-  }
-});
+//   }
+// });
