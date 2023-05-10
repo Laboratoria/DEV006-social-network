7b0f4efa31f711d2cdecffb500c78f834ba2200a
Mariana-Sanchez21 <marysansam21@gmail.com>
2023-05-10 18:44:22 -0500

    problema eslint firebase resuelto

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,20 +1,21 @@
 // aqui exportaras las funciones que necesites
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+
+import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBWvyr8kYQmm2JBFdVb-Wz8fn2W4omEhbU",
-  authDomain: "socialnetwork10-395ce.firebaseapp.com",
-  projectId: "socialnetwork10-395ce",
-  storageBucket: "socialnetwork10-395ce.appspot.com",
-  messagingSenderId: "272306479782",
-  appId: "1:272306479782:web:19575010f31b4558224a67",
-  measurementId: "G-1XF3847DV0"
+  apiKey: 'AIzaSyBWvyr8kYQmm2JBFdVb-Wz8fn2W4omEhbU',
+  authDomain: 'socialnetwork10-395ce.firebaseapp.com',
+  projectId: 'socialnetwork10-395ce',
+  storageBucket: 'socialnetwork10-395ce.appspot.com',
+  messagingSenderId: '272306479782',
+  appId: '1:272306479782:web:19575010f31b4558224a67',
+  measurementId: 'G-1XF3847DV0',
 };
 
 // Initialize Firebase
