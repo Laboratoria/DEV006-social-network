da251ee1bcf0315ded6e0f2a9595c1067176bfa2
Noemipurr <noebeatrizm@gmail.com>
2023-05-10 00:20:06 -0300

    Pequeñso cambios en el login

--- src/lib/login.js
+++ src/lib/login.js
@@ -1,4 +1,4 @@
-//import { loginFirebase } from ".";
+// import { loginFirebase } from ".";
 
 function login(navigateTo) {
   const section = document.createElement('section');
@@ -27,27 +27,26 @@ function login(navigateTo) {
   inputPass.placeholder = 'Password';
   inputPass.classList.add('password');
 
-  buttonLogin2.textContent = 'Login'
+  buttonLogin2.textContent = 'Login';
   buttonLogin2.classList.add('login2');
   buttonLogin2.addEventListener('click', () => {
-    navigateTo('/wall')
-    loginFirebase()
+    navigateTo('/wall');
+    // loginFirebase();
   });
-  buttonGoogle.textContent = 'continue with GOOGLE'
+  buttonGoogle.textContent = 'continue with GOOGLE';
   buttonGoogle.classList.add('google');
   buttonGoogle.addEventListener('click', () => {
-    navigateTo('/wall')
+    navigateTo('/wall');
   });
-  buttonReturn.textContent = 'Home'
+  buttonReturn.textContent = 'Home';
   buttonReturn.classList.add('return');
   buttonReturn.addEventListener('click', () => {
-    navigateTo('/')
+    navigateTo('/');
   });
 
   form.append(inputEmail, inputPass, buttonLogin2, buttonGoogle, buttonReturn);
   section.append(logo, title, caption, form);
   return section;
-
 }
 
 export default login;
