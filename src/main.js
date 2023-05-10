9839e233fefb8a0221802967f328511eb8015d3e
ValenParaguatey <valentina.paraguatey@gmail.com>
2023-05-10 17:53:40 -0400

    instalación de Firebase y prueba función de autenticación

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import './lib/signUp.js';
 
-myFunction();
+/*const formulario = document.querySelector("#signup-form");
+
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const email = document.querySelector("#signup-email").value;
+    const password = document.querySelector("#signup-password").value;
+    
+    auth
+   .createUserWithEmailAndPassword(email,password)
+   .then(userCredential => {
+    console.log('sign up');
+   })
+
+})*/
