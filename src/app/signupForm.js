d50e479ef6f784f508a1a29bfa28a3c8e699763c
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-05-10 19:15:52 -0600

    home

---
+++ src/app/signupForm.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+/* eslint-disable import/no-unresolved */
+// import { createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js';
+// import { auth } from './firebaseconfig.js';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const signupForm = document.querySelector('#signup-form');
+
+//   signupForm.addEventListener('submit', async (e) => {
+//     e.preventDefault();
+
+//     const email = signupForm['signup-email'].value;
+//     const password = signupForm['signup-password'].value;
+//     console.log(email, password);
+//     try {
+//       const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+//       console.log(userCredentials);
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   });
+// });
--- src/views/login.js
+++ src/views/login.js
@@ -1,23 +1,41 @@
-function home(navigateTo) {
+function login(navigateTo) {
   const section = document.createElement('section');
   const title = document.createElement('h2');
-  const btnLogin = document.createElement('button');
-  const btnRegister = document.createElement('button');
+  const buttonReturn = document.createElement('button');
+  const form = document.createElement('form');
+  const emailText = document.createElement('p');
+  const emailInput = document.createElement('input');
+  const passwordText = document.createElement('p');
+  const passwordInput = document.createElement('input');
+  const btnSignIn = document.createElement('button');
+  const forgotPassword = document.createElement('a');
 
-  btnLogin.textContent = 'Login';
-  btnLogin.addEventListener('click', () => {
-    navigateTo('/login');
+  emailText.textContent = 'Email:';
+  passwordText.textContent = 'Password:';
+  title.textContent = 'Login';
+  btnSignIn.textContent = 'Sign In';
+  forgotPassword.textContent = 'Forgot Password';
+
+  forgotPassword.href = '/forgotPassword';
+  forgotPassword.classList.add('btnForgotP');
+  forgotPassword.addEventListener('click', () => {
+    navigateTo('/forgotPassword');
   });
 
-  btnRegister.textContent = 'Register';
-  btnRegister.addEventListener('click', () => {
-    navigateTo('/register');
+  buttonReturn.textContent = 'Return to home';
+  buttonReturn.addEventListener('click', () => {
+    navigateTo('/');
   });
 
-  title.textContent = 'Bienevenido';
+  emailInput.placeholder = 'example@example.com';
+  emailInput.setAttribute('required', 'true');
+  passwordInput.placeholder = 'password';
+  passwordInput.setAttribute('required', 'true');
 
-  section.append(title, btnLogin, btnRegister);
+  form.append(emailText, emailInput, passwordText, passwordInput);
+  section.append(title, form, forgotPassword, btnSignIn, buttonReturn);
+
   return section;
 }
 
-export default home;
+export default login;
