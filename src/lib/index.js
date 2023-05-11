de3a9e818f489fe6a7378bd7849964b1595bf4ef
Gabriela Porras <gabriela24401@gmail.com>
2023-05-11 19:31:44 -0400

    limpieza de codigo

--- src/lib/index.js
+++ src/lib/index.js
@@ -29,17 +29,17 @@ const dataBase = getFirestore(app);
 export const colRef = collection(dataBase, 'post');
 
 // get collection data
-// getDocs(colRef)
-//   .then((snapshot) => {
-//     const posts = [];
-//     snapshot.docs.forEach((doc) => {
-//       posts.push({ ...doc.data(), id: doc.id });
-//     });
-//     /* console.log(posts); */
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//   });
+getDocs(colRef)
+  .then((snapshot) => {
+    const posts = [];
+    snapshot.docs.forEach((doc) => {
+      posts.push({ ...doc.data(), id: doc.id });
+    });
+    /* console.log(posts); */
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 // delete documents
 export const deletePost = (id) => {
