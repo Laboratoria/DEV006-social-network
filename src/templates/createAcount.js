8fab7c19c2df80480fb93d2a28e0e59e71ca3b03
RisellU <rcum22@gmail.com>
2023-05-12 17:07:01 -0500

    alertas para validar los campos

--- src/templates/createAcount.js
+++ src/templates/createAcount.js
@@ -7,9 +7,9 @@ function createAcount(navigateTo) {
   const title = document.createElement('h1');
   const caption = document.createElement('h2');
   const form = document.createElement('form');
+  const paragraph = document.createElement('p');
   const inputName = document.createElement('input');
   const inputEmail = document.createElement('input');
-  const paragraphEmail = document.createElement('p');
   const inputPass = document.createElement('input');
   const inputConfPass = document.createElement('input');
   const buttonSingUp = document.createElement('button');
@@ -55,9 +55,24 @@ function createAcount(navigateTo) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (emailRegex.test(e.target.value)) {
       console.log('pasa la validación');
-      paragraphEmail.textContent = '';
+      paragraph.textContent = '';
     } else {
-      paragraphEmail.textContent = 'Email no es valido';
+      paragraph.textContent = 'Email no es valido';
+    }
+  });
+  inputPass.addEventListener('change', (e) => {
+    if (e.target.value < 6) {
+      console.log(e.target.value, 'h');
+      paragraph.textContent = '';
+    } else {
+      paragraph.textContent = 'clave mayor a 6 carácteres';
+    }
+  });
+  inputConfPass.addEventListener('change', (e) => {
+    if (e.target.value === inputPass.value) {
+      paragraph.style.display = 'none';
+    } else {
+      paragraph.textContent = 'su clave no concuerda';
     }
   });
   // autenticar login
@@ -98,9 +113,9 @@ function createAcount(navigateTo) {
   // });
 
   form.append(
+    paragraph,
     inputName,
     inputEmail,
-    paragraphEmail,
     inputPass,
     inputConfPass,
     buttonSingUp,
