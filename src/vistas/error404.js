bf6d24b79a366dd3a40fd7f2a8f577ccc7067d86
Andrea Roa <andrearoa182@gmail.com>
2023-05-16 12:23:47 -0500

    aún sigue el error sin resolver, pero se realizaron cambios creando un js de error

--- src/vistas/error404.js
+++ src/vistas/error404.js
@@ -1,8 +1,8 @@
 // file error.js
-function error() {
+function error404() {
   const title = document.createElement('h2');
   title.textContent = 'Error 404 page no found, please go home';
   return title;
 }
 
-export default error;
+export default error404;
