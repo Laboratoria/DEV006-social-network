7bedb2111ed9076758bce66cd39d3a88e2bd1f01
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-05-18 18:28:26 -0600

    Ya quedo el contador de likes Yeeiii

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -7,7 +7,7 @@ import {
   addDoc,
   getDoc,
   updateDoc,
-  // getDocs,
+  getDocs,
   deleteDoc,
   onSnapshot,
   doc,
@@ -29,19 +29,71 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-const db = getFirestore();
+export const db = getFirestore();
 export const savePost = (postText) => {
   const user = auth.currentUser;
   const post = {
     name: user.displayName, // Obtén el nombre del usuario actual
     date: new Date().toLocaleDateString(), // Obtiene la fecha actual formateada
     post: postText,
+    likes: 0,
   };
 
   return addDoc(collection(db, 'publicaciones'), post);
 };
-// export const getPosts = () => getDocs(collection(db, 'publicaciones'));
+export const getPosts = () => getDocs(collection(db, 'publicaciones'));
 export const onGetPosts = (callback) => onSnapshot(collection(db, 'publicaciones'), callback);
 export const deletePost = (id) => deleteDoc(doc(db, 'publicaciones', id));
 export const getPost = (id) => getDoc(doc(db, 'publicaciones', id));
 export const updatePost = (id, newField) => updateDoc(doc(db, 'publicaciones', id), newField);
+// export const updateLikesCount = async (postId, userId) => {
+//   try {
+//     const postRef = doc(db, 'publicaciones', postId);
+//     const postSnapshot = await getDoc(postRef);
+//     const post = postSnapshot.data();
+
+//     // Verificar si el usuario ya dio like a la publicación
+//     if (post.likes && post.likes[userId]) {
+//       console.log('El usuario ya ha dado like a esta publicación.');
+//       return;
+//     }
+//     // Actualizar el contador de likes
+//     const updatedLikes = post.likes || {};
+//     updatedLikes[userId] = true;
+
+//     await updateDoc(postRef, { likes: updatedLikes });
+//     const likeBtn = document.getElementById(`contador-${postId}`);
+//     likeBtn.innerHTML = Object.keys(updatedLikes).length; // Mostrar la cantidad total de likes
+//   } catch (error) {
+//     console.error('Error al actualizar el recuento de "likes"', error);
+//   }
+// };
+// export const addLike = async (postId, userId) => {
+//   try {
+//     const postRef = doc(db, 'publicaciones', postId);
+//     const postSnapshot = await getDoc(postRef);
+//     const postData = postSnapshot.data();
+
+//     const updatedLikes = postData.likes || {};
+//     updatedLikes[userId] = true;
+
+//     await updateDoc(postRef, { likes: updatedLikes });
+//   } catch (error) {
+//     console.error('Error al añadir el like', error);
+//   }
+// };
+// export const removeLike = async (postId, userId) => {
+//   try {
+//     const postRef = doc(db, 'publicaciones', postId);
+//     const postSnapshot = await getDoc(postRef);
+//     const postData = postSnapshot.data();
+
+//     const updatedLikes = postData.likes || {};
+//     delete updatedLikes[userId];
+
+//     await updateDoc(postRef, { likes: updatedLikes });
+//   } catch (error) {
+//     console.error('Error al quitar el like', error);
+//   }
+// };
+export { getDoc, doc };
