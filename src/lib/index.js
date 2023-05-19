6051a7982d118842713a84b68e15e791de014c05
Marissa Vargas <marissa.lorena@hotmail.com>
2023-05-19 11:55:54 -0600

    limpieza y comentarios en index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,3 @@
-// aqui exportaras las funciones que necesites
-
 import { initializeApp } from 'firebase/app';
 import {
   getAuth,
@@ -19,116 +17,12 @@ import {
   // Se importa serveTimestamp para obtener fecha y hora del post
 } from 'firebase/firestore';
 import { firebaseConfig } from '../firebase.config.js';
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
 
+export const app = initializeApp(firebaseConfig); // Initialize Firebase
 export const auth = getAuth(app);
-
-// Initialize Firestore
-
-const dataBase = getFirestore(app);
-
-// collection reference
-export const colRef = collection(dataBase, 'post');
-
-// get collection data
-getDocs(colRef)
-  .then((snapshot) => {
-    const posts = [];
-    snapshot.docs.forEach((doc) => {
-      posts.push({ ...doc.data(), id: doc.id });
-    });
-    console.log(posts);
-    // posts.forEach((post) => {
-    //   console.log(post.like.length);
-    // });
-  })
-  .catch((error) => {
-    console.log(error.message);
-  });
-
-// Funcion get post para usar en wall
-const orderedQuery = query(colRef, orderBy('timestamp', 'desc'));
-export const getPost = (callback) => onSnapshot(orderedQuery, callback);
-
-// edit documents
-export const editPosts = (id, inputEditName, inputEditDescription) => {
-  const documentEditDoc = newdoc(colRef, id);
-  return updateDoc(documentEditDoc, {
-    petName: inputEditName,
-    description: inputEditDescription,
-    timestamp: serverTimestamp(),
-  })
-    .then(() => {
-      console.log('Documento actualizado correctamente');
-    })
-    .catch((error) => {
-      console.error('Error al actualizar el documento: ', error);
-    });
-};
-// delete documents
-export const deletePost = (id) => {
-  const documentDeleteDoc = newdoc(colRef, id);
-
-  return deleteDoc(documentDeleteDoc)
-    .then(() => {
-      console.log('Funciona Delete');
-    })
-    .catch(() => {
-      console.log('No funciona');
-    });
-};
-
-// adding documents
-export const addPost = (petName, petDescription) => {
-  const userName = getAuth().currentUser.displayName;
-  return addDoc(colRef, {
-    petName,
-    description: petDescription,
-    timestamp: serverTimestamp(), // definimos a timestamp para que se guarde en la colección
-    username: userName,
-    uid: getAuth().currentUser.uid,
-    like: [],
-  });
-};
-
-export const likePost = (id) => {
-  const post = newdoc(colRef, id);
-  return updateDoc(post, {
-    like: arrayUnion(auth.currentUser.uid),
-  });
-};
-
-export const dislikePost = (id) => {
-  const post = newdoc(colRef, id);
-  return updateDoc(post, {
-    like: arrayRemove(auth.currentUser.uid),
-  });
-};
-
-// Initialize Firebase Authentication and get a reference to the service
-
-// Fx para inicio de sesión con Google
-export const signPop = async (navigateTo) => {
-  const spanGoogle = document.getElementById('spanErrorGoogle');
-  const provider = new GoogleAuthProvider();
-  try {
-    // Ejecutar la autenticación con Google
-    const result = await signInWithPopup(auth, provider);
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    // Iniciar sesión en Firebase con las credenciales obtenidas
-    const userCredential = await signInWithCredential(auth, credential);
-    const user = userCredential.user;
-    navigateTo('/wall');
-    console.log(user);
-  } catch (error) {
-    const errorCode = error.code;
-    if (errorCode === 'auth/popup-closed-by-user') {
-      spanGoogle.textContent = 'No ha ingresado correctamente con google';
-    }
-    console.log(error);
-  }
-};
+const dataBase = getFirestore(app); // Initialize Firestore
+export const colRef = collection(dataBase, 'post'); // Referencia de la colección de "post"
+const orderedQuery = query(colRef, orderBy('timestamp', 'desc')); // Consulta la colección y la ordena los posts por su fecha/hora de publicación
 
 // Fx para crear usuarix
 export const createUser = async () => {
@@ -161,7 +55,29 @@ export const createUser = async () => {
   }
 };
 
-// Fx para iniciar sesión (ya registrado)
+// Fx para inicio de sesión con Google
+export const signPop = async (navigateTo) => {
+  const spanGoogle = document.getElementById('spanErrorGoogle');
+  const provider = new GoogleAuthProvider();
+  try {
+    // Ejecutar la autenticación con Google
+    const result = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    // Iniciar sesión en Firebase con las credenciales obtenidas
+    const userCredential = await signInWithCredential(auth, credential);
+    const user = userCredential.user;
+    navigateTo('/wall');
+    console.log(user);
+  } catch (error) {
+    const errorCode = error.code;
+    if (errorCode === 'auth/popup-closed-by-user') {
+      spanGoogle.textContent = 'No ha ingresado correctamente con google';
+    }
+    console.log(error);
+  }
+};
+
+// Fx para iniciar sesión
 export const LoginUser = (navigateTo) => {
   const emailInput = document.getElementById('txtEmail');
   const passwordInput = document.getElementById('txtPassword');
@@ -237,11 +153,83 @@ export const validateEmail = (email, spanErrorEmail) => {
 export const exit = (navigateTo) => {
   signOut(auth).then(() => {
     // Sign-out successful.
-  /* navigateTo para welcome */
     console.log('saliendo');
     navigateTo('/');
   }).catch((error) => {
     console.log(error);
     // An error happened.
+  });
+};
+
+// Obtiene los datos de nuestra colección
+getDocs(colRef)
+  .then((snapshot) => {
+    const posts = []; // array de nuestra colección
+    snapshot.docs.forEach((doc) => {
+      posts.push({ ...doc.data(), id: doc.id });
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+//  getPost permite obtener inmediatamente cambios en la colección utilizando una consulta ordenada.
+//  Cada vez que haya cambios, se ejecutará la fx callback para procesar los datos actualizados.
+export const getPost = (callback) => onSnapshot(orderedQuery, callback);
+
+// Fx para editar posts
+export const editPosts = (id, inputEditName, inputEditDescription) => {
+  const documentEditDoc = newdoc(colRef, id);
+  return updateDoc(documentEditDoc, {
+    petName: inputEditName,
+    description: inputEditDescription,
+    timestamp: serverTimestamp(),
+  })
+    .then(() => {
+      console.log('Documento actualizado correctamente');
+    })
+    .catch((error) => {
+      console.error('Error al actualizar el documento: ', error);
+    });
+};
+
+// Fx para eliminar posts
+export const deletePost = (id) => {
+  const documentDeleteDoc = newdoc(colRef, id); // Consulta la colección y el id del post
+  return deleteDoc(documentDeleteDoc)
+    .then(() => {
+      console.log('Funciona Delete');
+    })
+    .catch(() => {
+      console.log('No funciona');
+    });
+};
+
+// Fx para agregar publicaciones
+export const addPost = (petName, petDescription) => {
+  const userName = getAuth().currentUser.displayName;
+  return addDoc(colRef, {
+    petName,
+    description: petDescription,
+    timestamp: serverTimestamp(), // definimos a timestamp para que se guarde en la colección
+    username: userName,
+    uid: getAuth().currentUser.uid,
+    like: [], // aquí se guardarán los like
+  });
+};
+
+// Fx para dar like
+export const likePost = (id) => {
+  const post = newdoc(colRef, id);
+  return updateDoc(post, {
+    like: arrayUnion(auth.currentUser.uid), // arrayUnion agrega el elemento uid al array de likes
+  });
+};
+
+// Fx para quitar like
+export const dislikePost = (id) => {
+  const post = newdoc(colRef, id);
+  return updateDoc(post, {
+    like: arrayRemove(auth.currentUser.uid), // arrayRemove quita el elemento uid al array de likes
   });
 };
