40111ecd20b86599026fcc2eb9890e0cabfae776
Linda <lindalobo.aguilar@gmail.com>
2023-05-19 19:02:00 -0400

    conexion de post a firebase

--- src/templates/newpost.js
+++ src/templates/newpost.js
@@ -1,62 +1,81 @@
+import { saveTask } from '../lib/firebase.js'
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("works");
+});
+
 function newPost(navigateTo) {
-  const section = document.createElement('section');
-  const buttonReturn = document.createElement('button');
-  const titleNewPost = document.createElement('h1');
-  const form = document.createElement('form');
-  const paragraphImg = document.createElement('p');
-  const buttonPlus = document.createElement('img');
-  const paragraphTitle = document.createElement('p');
-  const textAreaTitle = document.createElement('textarea');
-  const paragraphReview = document.createElement('p');
-  const textAreaReview = document.createElement('textarea');
-  const buttonSave = document.createElement('button');
+  const section = document.createElement("section");
+  const buttonReturn = document.createElement("button");
+  const titleNewPost = document.createElement("h1");
+  const form = document.createElement("form");
+  const paragraphImg = document.createElement("p");
+  const buttonPlus = document.createElement("button");
+  const paragraphTitle = document.createElement("p");
+  const textAreaTitle = document.createElement("textarea");
+  const paragraphReview = document.createElement("p");
+  const textAreaReview = document.createElement("textarea");
+  const buttonSave = document.createElement("button");
 
-  titleNewPost.textContent = 'New Post';
-  titleNewPost.classList.add('titleNewPost');
-
-  paragraphImg.textContent = 'Add Image';
-  paragraphImg.classList.add('paragraphImg');
-
-  buttonPlus.classList.add('buttonPlus');
-
-  paragraphTitle.textContent = 'Add Title';
-  paragraphTitle.classList.add('paragraphTitle');
-
-  paragraphReview.textContent = 'Add Description';
-  paragraphReview.classList.add('paragraphReview');
-
-  textAreaTitle.classList.add('textAreaTitle');
-  textAreaReview.classList.add('textAreaReview');
+  section.setAttribute("id", "section");
+  textAreaTitle.setAttribute("id", "textAreaTitle");
+  textAreaReview.setAttribute("id", "textAreaReview");
   
-  buttonPlus.src = './img/icons-plus.png';
-  buttonPlus.classList.add('buttonPlus');
-  
-  buttonReturn.textContent = '.';
-  buttonReturn.classList.add('returnNewPost');
-  buttonReturn.addEventListener('click', () => {
-    navigateTo('/wall');
+  titleNewPost.textContent = "New Post";
+  titleNewPost.classList.add("titleNewPost");
+
+  paragraphImg.textContent = "Add Image";
+  paragraphImg.classList.add("paragraphImg");
+
+  buttonPlus.classList.add("buttonPlus");
+
+  paragraphTitle.textContent = "Add Title";
+  paragraphTitle.classList.add("paragraphTitle");
+
+  paragraphReview.textContent = "Add Description";
+  paragraphReview.classList.add("paragraphReview");
+
+  textAreaTitle.classList.add("textAreaTitle");
+  textAreaReview.classList.add("textAreaReview");
+
+  buttonReturn.textContent = ".";
+  buttonReturn.classList.add("returnNewPost");
+  buttonReturn.addEventListener("click", () => {
+    navigateTo("/wall");
   });
 
-  buttonSave.textContent = 'Save';
-  buttonSave.classList.add('save');
-  buttonSave.addEventListener('click', () => {
-    navigateTo('/wall');
-  });
+  buttonSave.textContent = "Save";
+  buttonSave.classList.add("save");
+  // buttonSave.addEventListener("click", () => {
+  //   navigateTo("/wall");
+  // });
 
   form.append(
     paragraphImg,
-    buttonPlus,
+    buttonSave,
     paragraphTitle,
     textAreaTitle,
     paragraphReview,
-    textAreaReview,
+    textAreaReview
   );
+  
+  section.append(buttonReturn, titleNewPost, form, buttonPlus);
 
-  section.append(buttonReturn, titleNewPost, form, buttonSave);
+  console.log(section)
+
+  section.addEventListener("submit",  (e) => {
+    e.preventDefault()
+    
+    const title = form['textAreaTitle'];
+    const description = form['textAreaReview'];
+
+    saveTask(title.value, description.value)
+  
+    form.reset()
+    
+  });
+
   return section;
 }
-export default newPost;
 
-// window.addEventListener('DOMcontentloaded', () => {
-//   console.log('works');
-// });
+export default newPost;
