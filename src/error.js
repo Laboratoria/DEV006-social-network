ef0d37fa087c26e577cd8924a215b861b278ca19
mabeliris <mabelle7623@gmail.com>
2023-05-30 12:19:14 -0400

    estructura base

---
+++ src/error.js
@@ -0,0 +1,8 @@
+
+function error() {
+    const title = document.createElement('h2');
+    title.textContent = 'Error 404 page no found, please go home';
+    return title;
+}
+
+export default error;
