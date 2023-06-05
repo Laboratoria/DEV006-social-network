58777cab4fa24ecd436a8a7be7f9780571b737e1
Dul <dul31389@gmail.com>
2023-06-05 17:25:42 -0500

    casi terminado home

--- src/SPA/components/registro.js
+++ src/SPA/components/registro.js
@@ -7,6 +7,7 @@ function registro(navigateTo) {
   const huellas = document.createElement('img');
   const sectionLogoR = document.createElement('div');
   const logoR = document.createElement('img');
+  const divDog = document.createElement('div');
   const dog = document.createElement('img');
   const sectionPets = document.createElement('div');
   const animalesR = document.createElement('img');
@@ -20,7 +21,7 @@ function registro(navigateTo) {
   const contenedorR = document.createElement('section');
   const textoEnlace = document.createElement('a');
   const textoRegister = document.createElement('p');
-
+  const footerRegister = document.createElement('footer');
   /* div lado izquierdo */
   imgFondo.classList.add('img-fondo');
   imgFondo.setAttribute('src', '../assets/vector.png');
@@ -28,39 +29,42 @@ function registro(navigateTo) {
   huellas.setAttribute('src', '../assets/huellasDiv.png');
   divDerecho.classList.add('div-rigth');
   divDerecho.append(imgFondo, huellas);
-
   logoR.classList.add('logoR');
   logoR.setAttribute('src', '../assets/logo1.png');
   dog.classList.add('perro');
   dog.setAttribute('src', '../assets/perrito.png');
+  divDog.classList.add('contain-dog');
+  divDog.append(dog);
   sectionLogoR.classList.add('seccionLogo-dog');
-  sectionLogoR.append(logoR, dog);
-
+  sectionLogoR.append(logoR, divDog);
   /* Textos etiquetas */
+  inputEmail.classList.add('inputs-registro');
   inputEmail.placeholder = 'Correo';
-  inputName.placeholder = 'Nombre Completo';
-  inputUser.placeholder = 'Nombre de Usuario';
+  inputPassword.classList.add('inputs-registro');
   inputPassword.placeholder = 'Contraseña';
+  inputName.classList.add('inputs-registro');
+  inputName.placeholder = 'Nombre Completo';
+  inputUser.classList.add('inputs-registro');
+  inputUser.placeholder = 'Nombre de Usuario';
+  inputConfirm.classList.add('inputs-registro');
   inputConfirm.placeholder = 'Confirmar contraseña';
   buttonCreate.textContent = 'Crear Cuenta';
-  textoRegister.textContent = '¿Ya tienes una cuenta?';
+  textoRegister.textContent = '¿Ya tienes cuenta?';
   textoEnlace.textContent = 'Iniciar Sesión';
-
   /* Imagen de animales */
   animalesR.classList.add('animalesR');
   animalesR.setAttribute('src', '../assets/pets.jpg');
-
   sectionPets.className = 'sectionPetsR';
-  buttonCreate.classList.add('login');
-  inputEmail.setAttribute('id', 'email');
-  inputPassword.setAttribute('id', 'password');
-
+  buttonCreate.classList.add('crearcuenta');
+  inputEmail.setAttribute('id', 'email2');
+  inputPassword.setAttribute('id', 'password2');
+  footerRegister.classList.add('footer-register');
+  footerRegister.textContent = '@WebDulValLeo';
   form.classList.add('formulario');
   form.append(inputEmail, inputPassword, inputName, inputUser, inputConfirm, buttonCreate);
-  contenedorR.classList.add('centrado');
-  textoEnlace.classList.add('button-registrate');
+  contenedorR.classList.add('centradoR');
+  textoEnlace.classList.add('button-ingresar');
   contenedorR.append(textoRegister, textoEnlace);
-
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     const email = inputEmail.value;
@@ -79,12 +83,10 @@ function registro(navigateTo) {
       form.appendChild(mensajeError);
     });
   });
-
   sectionPets.append(animalesR, form, contenedorR);
-
   section.classList.add('sectionGeneralR');
-  section.append(divDerecho, sectionPets, sectionLogoR);
-
+  section.append(divDerecho, sectionPets, sectionLogoR, footerRegister);
   return section;
 }
+
 export default registro;
