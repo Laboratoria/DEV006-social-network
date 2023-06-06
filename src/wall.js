49792df7bf28c660843ac007a32c6228f8674332
mabeliris <mabelle7623@gmail.com>
2023-06-06 13:37:31 -0400

    create-wall2

--- src/wall.js
+++ src/wall.js
@@ -5,12 +5,33 @@ function wall(navigateTo){
     const btnLogOut = document.createElement('button');
     const mainWall = document.createElement('main');
     const section1 = document.createElement('section');
-    const inputPost = document.createElement('div');
-    const newPost = document.createElement('input');
+    const newPost = document.createElement('div');
+    const inputPost = document.createElement("input");
     const btnPost = document.createElement('button');
-    const ctnComments= document.createElement('ul');
+    const ctnPost= document.createElement('ul');
+    const liPost= document.createElement ('li');
 
+    headerWall.setAttribute ("id", "header");
+    ctnHeader.setAttribute ('id','container-1');
+    imgHeader.setAttribute ('id','img-logo');
+    btnLogOut.setAttribute ('id','logOut');
+    mainWall.setAttribute ('id','mainWall');
+    section1.setAttribute ('id','section1');
+    newPost.setAttribute ('id','divNewPost');
+    inputPost.setAttribute ('id','inputNewPost');
+    ctnPost.setAttribute ('id','post');
+    liPost.setAttribute ('id','newPost');
+
+    inputPost.placeholder='¡Comparte tus eventos!'
     btnPost.textContent = 'PUBLICAR';
+
+    headerWall.append(imgHeader,btnLogOut);
+    mainWall.append (section1,newPost,inputPost,ctnPost,liPost);
+
+
+
+
+
 
 }
 export default wall; 
