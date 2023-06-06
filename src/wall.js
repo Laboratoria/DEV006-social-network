7fa159e0c054ce77a2237fd71b1a40cc59eadabb
ceciliape22 <118021663+ceciliape22@users.noreply.github.com>
2023-06-06 11:41:13 -0500

    create-wall

--- src/wall.js
+++ src/wall.js
@@ -1,4 +1,16 @@
 function wall(navigateTo){
-    
+    const headerWall = document.createElement('header');
+    const ctnHeader = document.createElement('div');
+    const imgHeader = document.createElement('img');
+    const btnLogOut = document.createElement('button');
+    const mainWall = document.createElement('main');
+    const section1 = document.createElement('section');
+    const inputPost = document.createElement('div');
+    const newPost = document.createElement('input');
+    const btnPost = document.createElement('button');
+    const ctnComments= document.createElement('ul');
+
+    btnPost.textContent = 'PUBLICAR';
+
 }
 export default wall; 
